535e974df87a8641fb6858868be1222db95bc162
CalmDeveloper <demir445@gmail.com>
2022-04-21 00:26:42 +0300

    cw5.1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
+import {useReducer} from "react";
 
 function App() {
-  return (
-    <div>
-hello
-    </div>
-  );
-}
-export default App;
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case 'inc':
+                return {...state, count: state.count + 1}
+
+            case 'dec':
+                return {...state, count: state.count - 1}
+            case 'reset':
+                return {...state, count: action.payload}
+            default:
+                return state
+
+        }
+    }
+        const [state, dispatch] = useReducer(reducer, {count: 0});
+        return (
+            <div>
+                <div>count: {state.count}</div>
+                <button onClick={() => dispatch({type: 'inc'})}>inc</button>
+                <button onClick={() => dispatch({type: 'dec'})}>dec</button>
+                <button onClick={() => dispatch({type: 'reset', payload: 0})}>reset</button>
+            </div>
+        );
+    }
+    export default App;
+
+
+
+
+
+
+
+
+
