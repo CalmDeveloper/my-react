ab5f7a724def87ab0af4495a142df09c468744b0
CalmDeveloper <demir445@gmail.com>
2022-04-22 17:47:47 +0300

    cw5.3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import './App.css';
+import {createRef, useReducer, useRef} from "react";
+
+
 
 function App() {
+
+
+    const catInp = createRef()
+    const reducer = (state,action) => {
+        const {type,payload} = action
+switch (type){
+    case 'addCat':
+        return {...state, cats:[...state.cats, {name:payload, id: Math.random(Math.round())}]}
+    case 'delCat':
+        return {...state, cats: [...state.cats.filter(cat =>cat.id !==payload )]}
+
+}
+    }
+const [state,dispatch] = useReducer(reducer,{cats:[]})
+
   return (
     <div>
-hello
+<div>
+    <label>
+        Cat:
+        <input type="text" ref={catInp}/>
+        <button onClick={()=>dispatch({type: 'addCat',payload:catInp.current.value})}>save</button>
+    </label>
+</div>
+
+<div>
+    {state.cats && state.cats.map(cat =>(<div key={cat.id}>
+        {cat.name} <button onClick={()=> dispatch({type:'delCat',payload:cat.id})}>delate</button>
+                                         </div>) ) }
+</div>
+
+
     </div>
   );
 }
