ddeeb4c288d4f1f4f5693377af7032a046526c21
CalmDeveloper <demir445@gmail.com>
2022-04-30 01:55:34 +0300

    8 hw+additional finish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import {Routes,Route,Navigate} from 'react-router-dom'
+import {MainLayout} from "./loyouts";
+import {EpisodePages} from "./pages/EpisodePages/EpisodePages";
+import {CharactersPage} from "./pages/CharactersPage/CharactersPage";
+import {NotFoundPage} from "./pages/NotFoundPage/NotFoundPage";
 
 function App() {
   return (
-    <div>
-hello
-    </div>
+    <Routes>
+
+<Route path={'/'} element={<MainLayout/>}>
+<Route index element={<Navigate to={'episodes'}/>}/>
+    <Route path={'episodes'} element={<EpisodePages/>}/>
+    <Route path={'characters'} element={<CharactersPage/>}/>
+    <Route path={'*'} element={<NotFoundPage/>}/>
+    </Route>
+
+
+    </Routes>
+
   );
 }
 export default App;
